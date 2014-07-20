cc823b60c5c7620ad34e36b85fad5e18bdd65e00
Alex Tang <atang930606@gmail.com>
2014-07-20 16:11:24 -0700

    init

---
+++ desktop/src/com/rhythmblocks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rhythmblocks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rhythmblocks.RhythmBlocks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RhythmBlocks(), config);
+	}
+}
